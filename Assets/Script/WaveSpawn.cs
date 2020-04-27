7f25b43de5179028d860f12e1f8653c3317ee1c1
lCassieL <denisandwork@gmail.com>
2020-04-27 13:48:14 +0300

    f c

---
+++ Assets/Script/WaveSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawn : MonoBehaviour
+{
+    public int WaveSize;
+    public GameObject EnemyPrefab;
+    public float enemyInterval;
+    public float startTime;
+    public Transform spawnPoint;
+    int enemyCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", startTime, enemyInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyCount = WaveSize) CancelInvoke("Spawn");
+    }
+    void Spawn()
+    {
+        enemyCount++;
+        GameObject enemy = GameObject.Instantiate(EnemyPrefab, spawnPoint.position, Quaternion.identity) as GameObject;
+    }
+}
